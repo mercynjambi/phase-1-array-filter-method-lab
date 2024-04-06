49ed32575727eb52c111a4057fc16e428ba0475f
mercynjambi <njambim695@gmail.com>
2024-04-06 11:16:52 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+
+
+function findMatching(drivers, query) {
+    
+    const lowerQuery = query.toLowerCase();
+    return drivers.filter(driver => driver.toLowerCase() === lowerQuery);
+  }
+  
+
+  function fuzzyMatch(drivers, query) {
+    
+    const lowerQuery = query.toLowerCase();
+    return drivers.filter(driver => driver.toLowerCase().startsWith(lowerQuery));
+  }
+  
+  
+  function matchName(drivers, query) {
+    return drivers.filter(driver => driver.name === query);
+  }
+  
